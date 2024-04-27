20464800260e09a061ccb48ee50ab22025bfda89
bcicc <tyler@saguarocm.com>
2024-04-27 10:36:12 -0700

    Ignore private registries, make downloading code multithreaded

--- download_code.jl
+++ download_code.jl
@@ -1,29 +1,38 @@
 using Base.Filesystem
 using JLD2
+using RegistryInstances
 using PackageAnalyzer
 using Pkg
 using ProgressMeter
 
-all_packages = find_packages()[4:end];
+const auth = PackageAnalyzer.github_auth(ENV["GITHUB_PAT"])
+
+registries = [i for i in reachable_registries() if i.name == "General"] # Ignore private registries
+all_packages = find_packages(; registries);
 all_packages = Any[i for i in all_packages if i.name ∉ ["SeisProcessing", "vOptGeneric", "ExactDiagonalization", "CachedFunctions", "Mads", "FractionalDelayFilter"]]
 push!(all_packages, "https://github.com/JuliaLang/julia")
 push!(all_packages, "https://github.com/JuliaLang/Distributed.jl")
 directory_path = "./tmp_code"
 
-all_results = PackageV1[]
-@showprogress for package in all_packages
-    res = analyze(package, root="./tmp_code")
-    push!(all_results, res)
+example_package = analyze(all_packages[1]; root="./tmp_code", auth=auth)
+all_results = [example_package for i in 1:length(all_packages)]
+p = Progress(length(all_packages))
+Threads.@threads for ix in 1:length(all_packages)
+    package = all_packages[ix]
+    res = analyze(package; root="./tmp_code", auth=auth)
+    all_results[ix] = res
+    next!(p)
 end
 
 # Download any packages that were skipped
 #TODO: figure out why some packages are skipped
+all_results = unique(all_results)
 current_packages = [i.name for i in all_results]
 @showprogress for package in all_packages
     if package.name in current_packages
         continue
     end
-    res = analyze(package, root="./tmp_code")
+    res = analyze(package; root="./tmp_code", auth)
     push!(all_results, res)
 end
 
